30de328ed21fa91f8eefe177f7203af8c08374d3
Hallez <jbhallez@aol.com>
2016-11-30 13:34:36 +0100

    Si vous êtes sur FREE mobile vous pourez envoyer des SMS par Jarvis —
    You: envoie un sms à léa
    Jarvis: Ok quel est le message ?
    You: as tu pensé à envoyer des fleurs à ta mère ?
    Jarvis: le message est as tu pense a envoyer des fleurs a ta mere  ok
    j'envoie ?
    You: oui
    Jarvis: c'est parti...

--- config.sh
+++ config.sh
@@ -1,16 +1,16 @@
 #voir API https://easydomoticz.com/nouveau-service-sms-pour-les-utilisateurs-de-free-mobile/
 # Exemple de résultat:
 # NOM1="Paul
-# USER1="123345678
+# USER1="123345678"
 # PASS1="vgdffFDRfggh"
 
-NOM1="JB"
-USER1="92278234"
-PASS1="vyDtDDnsE9g7js"
+NOM1=""
+USER1=""
+PASS1=""
 
-NOM2="LÃ©a"
-USER2="29091786"
-PASS2="p5tiTsmRW0NAfs"
+NOM2=""
+USER2=""
+PASS2=""
 
 NOM3=""
 USER3=""
