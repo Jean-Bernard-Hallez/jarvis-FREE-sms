7c542c6dbb5ad03d3d4bd88a2185036c880cf40e
Hallez <jbhallez@aol.com>
2016-12-03 07:41:28 +0100

    Si vous êtes sur FREE mobile vous pourez envoyer des SMS sur votre mobile !! par Jarvis
    Free Mobile a lance un service de notification via SMS pour ses abonnes,
    gratuit si vous avez le forfait illimite en sms bien sur. On peut
    maintenant s'envoyer des sms directement depuis Jarvis en appelant une
    simple url https dans les commands
    
    «
    https://smsapi.free-mobile.fr/sendmsg?user=000000pass=xxxxxxxxx&msg=Hello%20World%20!
    »
    
    Personnelement je l utilise deja avec DOMOTICZ, le principe est d
    activer le service SMS chez Free qui donne alors un password lie a votre
    identifiant Free mobile voir ici:
    https://easydomoticz.com/nouveau-service-sms-pour-les-utilisateurs-de-free-mobile/
    
    Jarvis-FREE-sms peut supporter jusqu a 5 utilisateurs Free mobile.

--- fr/functions.sh
+++ fr/functions.sh
@@ -2,60 +2,57 @@
 # Here you can create functions which will be available from the commands file
 # You can also use here user variables defined in your config file
 
-verimess () {
-[[ -z $(which curl) ]] && sudo apt-get install curl
-# echo "voici le sms: $(cat ~/smsa.txt)"
-MESS=`echo $(cat ~/smsa.txt) | sed "s/ /%20/g"`
-NOMOKA=`echo $(cat ~/smsa.txt) | cut -d"," -f1 | sed "s/,//g"` 
+verinoms () {
 
-NOM11=`echo $(jv_sanitize "$NOM1")`
-NOM22=`echo $(jv_sanitize "$NOM2")`
-NOM33=`echo $(jv_sanitize "$NOM3")`
-NOM44=`echo $(jv_sanitize "$NOM4")`
-NOM55=`echo $(jv_sanitize "$NOM5")`
+[[ -z $(which curl) ]] && sudo apt-get install curl
+USER=" "
+local NOMOKA=$order
+
+local NOM11=`echo $(jv_sanitize "$NOM1")`
+local NOM22=`echo $(jv_sanitize "$NOM2")`
+local NOM33=`echo $(jv_sanitize "$NOM3")`
+local NOM44=`echo $(jv_sanitize "$NOM4")`
+local NOM55=`echo $(jv_sanitize "$NOM5")`
 
 if [[ $NOMOKA == "$NOM11" ]] ; then
 USER=$USER1
 PASS=$PASS1
-envoimess
+PNOM="$order"
 return
 fi
 
 if [[ $NOMOKA == "$NOM22" ]] ; then
 USER=$USER2
 PASS=$PASS2
-envoimess
+PNOM="$order"
 return
 fi
 
 if [[ $NOMOKA == "$NOM33" ]] ; then
 USER=$USER3
 PASS=$PASS3
-envoimess
+PNOM="$order"
 return
 fi
 
 if [[ $NOMOKA == "$NOM44" ]] ; then
 USER=$USER4
 PASS=$PASS4
-envoimess
+PNOM="$order"
 return
 fi
 
 if [[ $NOMOKA == "$NOM55" ]] ; then
 USER=$USER5
 PASS=$PASS5
-envoimess
+PNOM="$order"
 return
 fi
 
-
-echo "Désolé message non envoyé, pas de nom $NOMOKA trouvé dans me fichier config point est ce hache"
-break
 return
 }
 
 envoimess () {
-echo "$(curl -s "https://smsapi.free-mobile.fr/sendmsg?user=$USER&pass=$PASS&msg=Mess%20envoyé%20avec%20$trigger%20pour:%20$MESS")"
-# echo "simul envoi ok"
+MESS=`echo $(cat ~/smsa.txt) | sed "s/ /%20/g"`
+echo "$(curl -s "https://smsapi.free-mobile.fr/sendmsg?user=$USER&pass=$PASS&msg=Mess%20envoyé%20avec%20$trigger:%20$MESS")"
 }
